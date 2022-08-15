aa81ac5207cdf675314a12fd8a6786a6eae0ba55
Kshitij Goel <kshitijg9899@gmail.com>
2022-08-15 17:35:02 +0530

    code refactored

--- script.js
+++ script.js
@@ -2,169 +2,118 @@ var symbols = ['X', 'O'];
 var board = [['.', '.', '.'], ['.', '.', '.'], ['.', '.', '.']];
 var wins = -1;
 var heads = document.getElementsByTagName("th");
-// var prevturn = 0;
-// var turn = prevturn;
 var turn = 0;
-var cmp = 0;
-function func(idd) {
-	// console.log(idd[1]);
-	var i = idd[1] - '0';
-	var j = idd[2] - '0';
-	if(board[i][j] == '.' && wins == -1) {
-		if(cmp == 1) {
-			turn = 0;
-			// prevturn = 0;
-			console.log(cmp);
-			document.getElementById(idd).innerText = symbols[turn];
+var comp_enabled = 0;
+var result = document.getElementById("winner");
+var p1sc = document.getElementById("p1score");
+var p2sc = document.getElementById("p2score");
+var all = document.getElementsByClassName("box");
+var mode = document.getElementById("mode");
+
+function place_sym(id) {
+	var i = id[1] - '0';
+	var j = id[2] - '0';
+	if(board[i][j] == '.' && wins === -1) {
+		if(comp_enabled == 1) {
+			document.getElementById(id).innerText = symbols[turn];
 			board[i][j] = symbols[turn];
-			var ans = check(symbols[turn]);
+			var ans = check_game(symbols[turn]);
 			var d = draw();
 			if(ans === 0) {
 				wins = 0;
-				setTimeout(function() {
-				  	alert("Congratulations, Player won.");
-				}, 10);
-				setTimeout(updsc, 100);
-				setTimeout(reset, 100);
+				result.innerText = "Player Won!"
+				update_score();
 			} else if(d == 1) {
-				setTimeout(function() {
-				  	alert("Oops, It's a Draw.");
-				}, 10);
-				setTimeout(updsc, 100);
-				setTimeout(reset, 100);
+				result.innerText = "It's a draw!"
 			} else {
 				computerChance();
-				ans = check('O');
+				ans = check_game('O');
 				d = draw();
 				if(ans === 1) {
 					wins = 1;
-					setTimeout(function() {
-					  	alert("Oops, Computer won.");
-					}, 10);
-					setTimeout(updsc, 100);
-					setTimeout(reset, 100);
+					result.innerText = "Computer Won!"
+					update_score();
 				} else if(d === 1) {
-					setTimeout(function() {
-					  	alert("Oops, It's a Draw.");
-					}, 10);
-					setTimeout(updsc, 100);
-					setTimeout(reset, 100);
+					result.innerText = "It's a draw!"
 				}
 			}
 		} else {
-			// console.log(turn);
-			document.getElementById(idd).innerText = symbols[turn];
-			// document.getElementById(idd).setAttribute("class", "styl");
-			console.log(heads[0]);
-			if(turn == 0) {
-				heads[1].classList.add("styl");
-				heads[0].classList.remove("styl");
-			} else {
-				heads[0].classList.add("styl");
-				heads[1].classList.remove("styl");
-			}
+			document.getElementById(id).innerText = symbols[turn];
 			board[i][j] = symbols[turn];
-			var ans = check(symbols[turn]);
+			var ans = check_game(symbols[turn]);
 			var d = draw();
-			turn ^= 1;
-			if(ans === 0) { //player 1 wins
+			if(ans === 0) {
 				wins = 0;
-				heads[0].classList.remove("styl");
-				heads[1].classList.remove("styl");
-				heads[0].classList.add("styl");
-				setTimeout(function() {
-				  	alert("Congratulations, Player 1 won.");
-				}, 10);
-				setTimeout(updsc, 100);
-				setTimeout(reset, 100);
-			} else if(ans === 1) { //player 2 wins
-				heads[0].classList.remove("styl");
-				heads[1].classList.remove("styl");
-				heads[1].classList.add("styl");
+				result.innerText = "Player 1 Won!"
+				update_score();
+			} else if(ans === 1) {
 				wins = 1;
-				setTimeout(function() {
-				  	alert("Congratulations, Player 2 won.");
-				}, 10);
-				setTimeout(updsc, 100);
-				setTimeout(reset, 100);
+				result.innerText = "Player 2 Won!"
+				update_score();
 			} else if(d === 1) {
-				heads[0].classList.remove("styl");
-				heads[1].classList.remove("styl");
-				setTimeout(function() {
-				  	alert("It's a Draw.");
-				}, 10);
-				setTimeout(updsc, 100);
-				setTimeout(reset, 100);
+				result.innerText = "It's a draw!"
+			} else {
+				if(turn == 0) {
+					heads[1].classList.add("chance");
+					heads[0].classList.remove("chance");
+				} else {
+					heads[0].classList.add("chance");
+					heads[1].classList.remove("chance");
+				}
 			}
+			turn ^= 1;
 		}
 	}
 }
 
-function check(p) {
-	// console.log(p);
+function draw_winning_line(id1, id2, id3) {
+	document.getElementById(id1).classList.add("won");
+	document.getElementById(id2).classList.add("won");
+	document.getElementById(id3).classList.add("won");
+}
+
+function who_won(sym) {
+	if(sym === 'X') return 0; // player 1 won
+	else return 1; // player 2 won
+}
+
+function check_game(sym) {
 	var flag = 0;
 	for(var i=0;i<3;i++) {
-		if(board[i][0] === board[i][1] && board[i][1] === board[i][2] && board[i][2] === p) {
+		if(board[i][0] === board[i][1] && board[i][1] === board[i][2] && board[i][2] === sym) {
 			var id1 = "c" + i + 0, id2 = "c" + i + 1, id3 = "c" + i + 2;
-			document.getElementById(id1).classList.add("styl");
-			document.getElementById(id2).classList.add("styl");
-			document.getElementById(id3).classList.add("styl");
+			draw_winning_line(id1, id2, id3);
 			flag = 1;
 			break;
 		}
 	}
-	if(flag === 1) {
-		if(p === 'X') {
-			return 0;
-		} else {
-			return 1;
-		}
-	}
+	if(flag === 1) return who_won(sym);
+
 	for(var i=0;i<3;i++) {
-		if(board[0][i] === board[1][i] && board[1][i] === board[2][i] && board[2][i] === p) {
+		if(board[0][i] === board[1][i] && board[1][i] === board[2][i] && board[2][i] === sym) {
 			var id1 = "c" + 0 + i, id2 = "c" + 1 + i, id3 = "c" + 2 + i;
-			document.getElementById(id1).classList.add("styl");
-			document.getElementById(id2).classList.add("styl");
-			document.getElementById(id3).classList.add("styl");
+			draw_winning_line(id1, id2, id3);
 			flag = 1;
 			break;
 		}
 	}
-	if(flag === 1) {
-		if(p === 'X') {
-			return 0;
-		} else {
-			return 1;
-		}
-	}
-	if(board[0][0] === board[1][1] && board[1][1] === board[2][2] && board[2][2] === p) {
+	if(flag === 1) return who_won(sym);
+
+	if(board[0][0] === board[1][1] && board[1][1] === board[2][2] && board[2][2] === sym) {
 		var id1 = "c" + 0 + 0, id2 = "c" + 1 + 1, id3 = "c" + 2 + 2;
-		document.getElementById(id1).classList.add("styl");
-		document.getElementById(id2).classList.add("styl");
-		document.getElementById(id3).classList.add("styl");
+		draw_winning_line(id1, id2, id3);
 		flag = 1;
 	}
-	if(flag === 1) {
-		if(p === 'X') {
-			return 0;
-		} else {
-			return 1;
-		}
-	}
-	if(board[0][2] === board[1][1] && board[1][1] === board[2][0] && board[2][0] === p) {
+	if(flag === 1) return who_won(sym);
+
+	if(board[0][2] === board[1][1] && board[1][1] === board[2][0] && board[2][0] === sym) {
 		var id1 = "c" + 0 + 2, id2 = "c" + 1 + 1, id3 = "c" + 2 + 0;
-		document.getElementById(id1).classList.add("styl");
-		document.getElementById(id2).classList.add("styl");
-		document.getElementById(id3).classList.add("styl");
+		draw_winning_line(id1, id2, id3);
 		flag = 1;
 	}
-	if(flag === 1) {
-		if(p === 'X') {
-			return 0;
-		} else {
-			return 1;
-		}
-	}
+	if(flag === 1) return who_won(sym);
+
+	return -1; // nobody won
 }
 
 function computerChance() {
@@ -180,39 +129,20 @@ function computerChance() {
 	}
 }
 
-function updsc() {
+function update_score() {
 	var psc1, psc2, pi1, pi2;
 	if(wins == 0) {
-		psc1 = document.getElementById("p1score");
-		psc2 = document.getElementById("p2score");
-		pi1 = psc1.innerText;
-		pi2 = psc2.innerText;
+		p1sc.innerText = (p1sc.innerText - '0') + 1;
 	} else if(wins == 1) {
-		psc1 = document.getElementById("p2score");
-		psc2 = document.getElementById("p1score");
-		pi1 = psc1.innerText;
-		pi2 = psc2.innerText;
-	} else {
-		psc1 = document.getElementById("p1score");
-		psc2 = document.getElementById("p2score");
-		pi1 = psc1.innerText;
-		pi2 = psc2.innerText;
-	}
-	if(wins != -1) {
-		psc1.innerText = (pi1 - '0') + 1;
-		psc2.innerText = pi2;
-	} else {
-		psc1.innerText = pi1;
-		psc2.innerText = pi2;
+		p2sc.innerText = (p2sc.innerText - '0') + 1;
 	}
 }
 
 function reset() {
-	var all = document.getElementsByClassName("box");
-	// console.log(all);
+	winner.innerText = "";
 	for(var i=0;i<all.length;i++) {
 		all[i].innerText = '';
-		all[i].classList.remove("styl");
+		all[i].classList.remove("won");
 	}
 	for(var i=0;i<3;i++) {
 		for(var j=0;j<3;j++) {
@@ -220,21 +150,9 @@ function reset() {
 		}
 	}
 	wins = -1;
-	// prevturn ^= 1;
-	// turn = prevturn;
 	turn = 0;
-	if(cmp == 1) {
-		// prevturn = 0;
-		// turn = prevturn;
-		turn = 0;
-	}
-	if(turn == 0) {
-		heads[0].classList.add("styl");
-		heads[1].classList.remove("styl");
-	} else {
-		heads[1].classList.add("styl");
-		heads[0].classList.remove("styl");
-	}
+	heads[0].classList.add("chance");
+	heads[1].classList.remove("chance");
 }
 
 function draw() {
@@ -248,46 +166,26 @@ function draw() {
 	return 1;
 }
 
-function change() {
-	var comp = document.getElementById("namech");
-	if(comp.innerText == "MULTIPLAYER?") {
-		comp.innerText = "PLAY WITH COMPUTER?";
-		heads[1].innerText = "PLAYER 2 (O)";
+function mode_change() {
+	if(mode.innerText == "MULTIPLAYER?") {
+		mode.innerText = "PLAY WITH COMPUTER?";
 		heads[0].innerText = "PLAYER 1 (X)";
-		cmp = 0;
+		heads[1].innerText = "PLAYER 2 (O)";
+		comp_enabled = 0;
 	} else {
-		comp.innerText = "MULTIPLAYER?";
-		heads[1].innerText = "COMPUTER (O)";
+		mode.innerText = "MULTIPLAYER?";
 		heads[0].innerText = "PLAYER (X)";
-		cmp = 1;
+		heads[1].innerText = "COMPUTER (O)";
 		heads[0].classList.add("styl");
 		heads[1].classList.remove("styl");
+		comp_enabled = 1;
 	}
 	wins = -1;
-	fullrst();
+	full_reset();
 }
 
-function fullrst() {
-	var all = document.getElementsByClassName("box");
-	// console.log("heeee");
-	// console.log(all);
-	for(var i=0;i<all.length;i++) {
-		all[i].innerText = '';
-		all[i].classList.remove("styl");
-	}
-	for(var i=0;i<3;i++) {
-		for(var j=0;j<3;j++) {
-			board[i][j] = '.';
-		}
-	}
-	heads[0].classList.remove("styl");
-	heads[1].classList.remove("styl");
-	var psc1 = document.getElementById("p1score");
-	var psc2 = document.getElementById("p2score");
-	psc1.innerText = 0;
-	psc2.innerText = 0;
-	// prevturn = 0;
-	// turn = prevturn;
-	turn = 0;
-	heads[0].classList.add("styl");
+function full_reset() {
+	reset();
+	p1sc.innerText = 0;
+	p2sc.innerText = 0;
 }
